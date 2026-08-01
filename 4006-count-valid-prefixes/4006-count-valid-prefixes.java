1d44291ef79588c0ea56792a532850933944fef3
TanishqSajwan <tanishqsajwan266@gmail.com>
2026-08-01 22:59:41 +0530

    Time: 1 ms (100%), Space: 43.5 MB (99.92%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int one = 0;
+        int zero = 0;
+        int  ans = 0 ;
+          for(int i = 0 ; i< s.length() ; i++){
+              if(s.charAt(i)=='0') zero++ ;
+              else  one++;
+
+              if(Math.abs(zero-one)<=1) ans++;
+          }
+        return ans++;
+    }
+}
