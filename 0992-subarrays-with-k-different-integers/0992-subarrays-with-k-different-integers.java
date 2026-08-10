9c602728f3c8702f63d81d6352628d19b218d38e
TanishqSajwan <tanishqsajwan266@gmail.com>
2026-08-10 22:04:10 +0530

    Time: 50 ms (39.82%), Space: 50.3 MB (43.72%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+    return co(nums,k)-co(nums,(k-1));             
+}
+public static int co(int[] nums , int k){
+HashMap<Integer,Integer> freq = new HashMap<>();
+    int left= 0 ,count = 0;
+    for(int right = 0 ;right < nums.length ; right++){
+        freq.put(nums[right],freq.getOrDefault(nums[right],0)+1);
+
+        while(freq.size()>k){
+            freq.put(nums[left],freq.getOrDefault(nums[left],0)-1);
+            if(freq.get(nums[left])==0) freq.remove(nums[left]);
+            left++;
+        }
+        count +=right-left+1;
+        
+    }
+   return count;
+    }
+}
